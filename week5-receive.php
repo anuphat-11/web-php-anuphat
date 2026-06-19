2a3eb4f02b474553a77e7e0b8d50bb21f295ef6a
anuphat-11 <dsion2463@gmail.com>
2026-06-19 12:56:09 +0700

    week5

---
+++ week5-receive.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_GET['userName'];
+    $password = $_GET['userPass'];
+    $email = $_GET['userEmail'];
+    $number = $_GET['userAge'];
+    $birth = $_GET['userBirthday'];
+    $gender = $_GET['UserGender'];
+    $city = $_GET['userCity'];
+    $hobby = $_GET['userHobby'];
+
+
+    echo "สวัสดีคุณ $name<br>";
+    echo "รหัสผ่านของคุณคือ: $password<br>";
+    echo "อีเมลของคุณคือ: $email<br>";
+    echo "อายุของคุณคือ: $number<br>";
+    echo "วันเกิดของคุณคือ: $birth<br>";
+    echo "เพศของคุณคือ: $gender<br>";
+    echo "จังหวัดของคุณคือ: $city<br>";
+    echo "งานอดิเรกของคุณคือ: " . implode(", ", $hobby) . "<br>";
+
+?>
