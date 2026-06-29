c77d817877848e7174454c368453625724dd85fb
anuphat-11 <dsion2463@gmail.com>
2026-06-29 16:53:07 +0700

    Update week7-valid.php

--- week7-valid.php
+++ week7-valid.php
@@ -3,14 +3,14 @@ $mial = "123@aaa.com";
 $age = 20;
 
 if (filter_var($mial, FILTER_VALIDATE_EMAIL)) {
-    echo "อีเมลถูกต้อง";
+    echo "อีเมลถูกต้อง". "<br>";
 } else {
-    echo "อีเมลไม่ถูกต้อง";
+    echo "อีเมลไม่ถูกต้อง". "<br>";
 }
 
 IF (filter_var($age, FILTER_VALIDATE_INT)) {
-    echo "อายุถูกต้อง";
+    echo "อายุถูกต้อง". "<br>";
 } else {
-    echo "อายุไม่ถูกต้อง";
+    echo "อายุไม่ถูกต้อง". "<br>";
 }
 ?>
