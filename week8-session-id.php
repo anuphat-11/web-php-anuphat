10509e7538b2111f9648c8c4bfb1d14ad86e9842
anuphat-11 <dsion2463@gmail.com>
2026-07-10 11:23:03 +0700

    .

---
+++ week8-session-id.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    echo"Session ID: " . session_id();
+    
+    session_regenerate_id(true);
+    echo"<br>New Session ID: " . session_id();
+
+?>
---
+++ week8-session-login.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+
+    $_SESSION["username"] = "anphat";
+
+    echo "ล็อคอินสำเร็จ ";
+
+?>
