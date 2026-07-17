f3fec801510736880f8d178c0bb520bbfe61447d
anuphat-11 <dsion2463@gmail.com>
2026-07-17 11:50:13 +0700

    .

--- week9-connectDatabase.php
+++ week9-connectDatabase.php
@@ -5,21 +5,20 @@
     $password = "";
 
     try{
-    $pdo = new pdo(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4", 
-        $username, 
-        $password
-    );
+        $pdo = new pdo(
+           "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            $username,
+            $password
+        );
 
-    $pdo->setAttribute(
-        PDO::ATTR_ERRMODE, 
-        PDO::ERRMODE_EXCEPTION
-    );
+        $pdo->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
 
 
         echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
     }catch(PDOException $e){
         echo "เกิดข้อผิดพลาด : " . $e->getMessage();
-      
     }
 ?>
